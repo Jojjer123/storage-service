3d2226f659b40513955be4352e309063a570b3d6
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-15 16:30:45 +0100

    Change response & config format.

--- pkg/data_store/data_store.go
+++ pkg/data_store/data_store.go
@@ -6,18 +6,30 @@ import (
 	"path/filepath"
 
 	"github.com/openconfig/gnmi/proto/gnmi"
+	"gopkg.in/yaml.v2"
 )
 
-// type Config struct {
-// 	DevicesWithMonitoring []struct {
-// 		Device         interface{} `yaml:"device"`
-// 		DeviceCounters []struct {
-// 			Name     string `yaml:"name"`
-// 			Interval int    `yaml:"interval"`
-// 			Path     string `yaml:"path"`
-// 		} `yaml:"device_counters"`
-// 	} `yaml:"devices_with_monitoring"`
-// }
+type Config struct {
+	DevicesWithMonitoring []struct {
+		DeviceIP      string `yaml:"device_ip"`
+		DeviceName    string `yaml:"device_name"`
+		Protocol      string `yaml:"protocol"`
+		DefaultConfig struct {
+			DeviceCounters []struct {
+				Name     string `yaml:"name"`
+				Interval int    `yaml:"interval"`
+				Path     string `yaml:"path"`
+			} `yaml:"device_counters"`
+		} `yaml:"default_config"`
+		TemporaryConfig struct {
+			DeviceCounters []struct {
+				Name     string `yaml:"name"`
+				Interval int    `yaml:"interval"`
+				Path     string `yaml:"path"`
+			} `yaml:"device_counters"`
+		} `yaml:"temporary_config"`
+	} `yaml:"devices_with_monitoring"`
+}
 
 func GetConfig(req *gnmi.GetRequest) []byte {
 	filename, _ := filepath.Abs("./monitoring_config/example_config.yaml")
@@ -28,12 +40,43 @@ func GetConfig(req *gnmi.GetRequest) []byte {
 		fmt.Println("Failed to read file")
 	}
 
-	// var config Config
-	// fmt.Println(yaml.Unmarshal(config_file, &config))
+	var config Config
+	yaml.Unmarshal(config_file, &config)
+
+	// fmt.Println(config)
+
+	index := -1
+	for i, device := range config.DevicesWithMonitoring {
+		if device.DeviceIP == req.Path[0].Target {
+			index = i
+			break
+		}
+	}
+
+	var conf []byte
+
+	if index != -1 {
+		conf, err = yaml.Marshal(config.DevicesWithMonitoring[index])
+	} else {
+		fmt.Println("Could not find the device in config!")
+	}
+
+	if err != nil {
+		fmt.Println("Failed to convert config to byte slice")
+	}
+
+	return conf
+
+	// test := []byte(fmt.Sprintf("%v", config))
+
+	// var conf Config
+	// yaml.Unmarshal(test, &conf)
+	// fmt.Println(conf.DevicesWithMonitoring[0])
 
 	// fmt.Println(config)
 
 	// return []byte(fmt.Sprintf("%v", config))
 
-	return config_file
+	// return nil
+	// return config_file
 }
