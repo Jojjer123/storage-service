8643edcd2a958064484dc1b60a737b049b11503c
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-17 09:15:49 +0100

    Add multi-config for each device.

--- pkg/data_store/data_store.go
+++ pkg/data_store/data_store.go
@@ -11,23 +11,16 @@ import (
 
 type Config struct {
 	DevicesWithMonitoring []struct {
-		DeviceIP      string `yaml:"device_ip"`
-		DeviceName    string `yaml:"device_name"`
-		Protocol      string `yaml:"protocol"`
-		DefaultConfig struct {
+		DeviceIP   string `yaml:"device_ip"`
+		DeviceName string `yaml:"device_name"`
+		Protocol   string `yaml:"protocol"`
+		Configs    []struct {
 			DeviceCounters []struct {
 				Name     string `yaml:"name"`
 				Interval int    `yaml:"interval"`
 				Path     string `yaml:"path"`
 			} `yaml:"device_counters"`
-		} `yaml:"default_config"`
-		TemporaryConfig struct {
-			DeviceCounters []struct {
-				Name     string `yaml:"name"`
-				Interval int    `yaml:"interval"`
-				Path     string `yaml:"path"`
-			} `yaml:"device_counters"`
-		} `yaml:"temporary_config"`
+		} `yaml:"configs"`
 	} `yaml:"devices_with_monitoring"`
 }
 
